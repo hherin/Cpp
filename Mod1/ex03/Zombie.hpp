0bb8c10f1b87d75bbdab31633c48e5e675c12dbb
hherin <hherin@student.42.fr>
2020-12-03 13:04:49 +0100

    add header

--- Mod1/ex03/Zombie.hpp
+++ Mod1/ex03/Zombie.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Zombie.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heleneherin <heleneherin@student.42.fr>    +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 15:21:22 by heleneherin       #+#    #+#             */
-/*   Updated: 2020/12/02 19:06:26 by heleneherin      ###   ########.fr       */
+/*   Updated: 2020/12/03 13:04:18 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 class Zombie
 {
