81b439615299d3116339c2f471c27ac414b9dcfb
hherin <hherin@student.42.fr>
2020-12-03 13:13:57 +0100

    gtg

--- Mod1/ex07/fileChecker.cpp
+++ Mod1/ex07/fileChecker.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fileChecker.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heleneherin <heleneherin@student.42.fr>    +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 00:48:17 by heleneherin       #+#    #+#             */
-/*   Updated: 2020/12/03 12:48:34 by heleneherin      ###   ########.fr       */
+/*   Updated: 2020/12/03 13:13:13 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,20 @@ int fileClass::fileInBuf(void)
 	std::ifstream fich(filename.c_str());
 	std::stringstream buf; // variable contenant l'intégralité du fichier
 	std::string line;
+	std::string repFilename;
 	std::size_t found;
 
 	if (!fich){
 		std::cout << "Can't open the file\n";
 		return 0;
 	}
-	std::ofstream outfile (filename.append("replace"));
+	repFilename = filename.append("replace");
+	std::ofstream outfile(repFilename.c_str());
 	buf << fich.rdbuf(); // copier l'intégralité du fichier dans le buffer
 	line = buf.str();
-	while ((found = line.find(rep1.c_str())) != std::string::npos){
+	while ((found = line.find(rep1.c_str())) != std::string::npos)
 		line.replace(found, rep1.length(), rep2.c_str());
-	}
-	outfile << line << std::endl;
+	outfile << line;
 	outfile.close();
 	return (1);
 }
