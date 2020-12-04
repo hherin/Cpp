ff84f47e511c485286ac0c3007ebd90007f3ff36
hherin <hherin@student.42.fr>
2020-12-04 15:39:55 +0100

    ex01 done

---
+++ Mod2/ex01/Fixed.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heleneherin <heleneherin@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/04 11:16:17 by heleneherin       #+#    #+#             */
+/*   Updated: 2020/12/04 15:38:05 by heleneherin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(): fixedPoint(0)
+{
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed &cp)
+{
+	std::cout << "Copy constructor called\n";
+	fixedPoint = cp.fixedPoint;
+}
+
+Fixed::Fixed(const int nb)
+{
+	fixedPoint = nb << fractBit;
+}
+
+Fixed::Fixed(const float nb)
+{
+	fixedPoint = nb * (float)(1 << fractBit);
+}
+
+Fixed& Fixed::operator=(const Fixed &cp)
+{
+	std::cout << "Assignation operator called\n";
+	if (this == &cp)
+		return (*this);
+	fixedPoint = cp.getRawBits();
+	return (*this);
+}
+
+std::ostream& operator<<(std::ostream &st, const Fixed &cp)
+{
+	st << cp.toFloat();
+	return (st);
+}
+
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called\n";
+	return fixedPoint;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	fixedPoint = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return (((float)fixedPoint / (float)(1 << fractBit)));
+}
+
+int Fixed::toInt(void) const
+{
+	return (fixedPoint / (1 << fractBit));
+}
---
+++ Mod2/ex01/Fixed.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heleneherin <heleneherin@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/04 11:01:38 by heleneherin       #+#    #+#             */
+/*   Updated: 2020/12/04 15:33:05 by heleneherin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const Fixed &);
+		Fixed(const int);
+		Fixed(const float);
+		Fixed& operator=(const Fixed &);
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+
+	private:
+		int fixedPoint;
+		static const int fractBit = 8;
+};
+
+std::ostream& operator<<(std::ostream&, const Fixed&);
+
+#endif
